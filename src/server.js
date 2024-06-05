583b82fcd7a65ecc9918ea2fa4ca0e79bdc74eb0
DenisPepper <thech@yandex.ru>
2024-06-05 15:30:01 +0300

    Добавит обработчик при отключении пользователя

--- src/server.js
+++ src/server.js
@@ -11,13 +11,16 @@ const io = new Server(server);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+    res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+    console.log('server running at http://localhost:3000');
 });
