9628929bef26a066da0201edf3553938274d4a24
DenisPepper <thech@yandex.ru>
2024-06-26 21:00:25 +0300

    Добавит компоненты и роутинг для тренировки crud операций

---
+++ src/layouts/crud-training-layout.jsx
@@ -0,0 +1,34 @@
+import {
+  Link,
+  Outlet,
+  ScrollRestoration,
+  useNavigation,
+} from 'react-router-dom';
+
+export function CrudTrainingLayout() {
+  const { state } = useNavigation();
+
+  return (
+    <>
+      <h1>Тренировка crud-операций на supabase</h1>
+      <nav>
+        <ul style={{ display: 'flex', listStyle: 'none', gap: '20px' }}>
+          <li>
+            <Link to='/create'>create</Link>
+          </li>
+          <li>
+            <Link to='/read'>read</Link>
+          </li>
+          <li>
+            <Link to='/update'>update</Link>
+          </li>
+          <li>
+            <Link to='/delete'>delete</Link>
+          </li>
+        </ul>
+      </nav>
+      <ScrollRestoration />
+      <div>{state === 'loading' ? <div>Loading...</div> : <Outlet />}</div>
+    </>
+  );
+}
---
+++ src/routers/crud-training-router.jsx
@@ -0,0 +1,22 @@
+import { Navigate, createBrowserRouter } from 'react-router-dom';
+import { CrudTrainingLayout } from '../layouts/crud-training-layout';
+import { Home } from '../components/crud-training/home';
+import { Create } from '../components/crud-training/create';
+import { Read } from '../components/crud-training/read';
+import { Update } from '../components/crud-training/update';
+import { Delete } from '../components/crud-training/delete';
+
+export const crudTrainingRouter = createBrowserRouter([
+  {
+    element: <CrudTrainingLayout />,
+    path: '/',
+    children: [
+      { index: true, element: <Home /> },
+      { path: '*', element: <Navigate to='/' /> },
+      { path: 'create', element: <Create /> },
+      { path: 'read', element: <Read /> },
+      { path: 'update', element: <Update /> },
+      { path: 'delete', element: <Delete /> },
+    ],
+  },
+]);
