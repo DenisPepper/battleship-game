3fdfb7fd6270da79c0b22c78b3f73ce0f99b26fb
DenisPepper <thech@yandex.ru>
2024-07-16 20:38:26 +0300

    Добавит резервный контент для доступности

--- src/components/canvas-training/home.jsx
+++ src/components/canvas-training/home.jsx
@@ -3,7 +3,7 @@ import './home.css';
 export function Home() {
   return (
     <canvas className='canvas-container' width={500} height={500}>
-      
+      A visual design tool for the interior filling of the cabinet.
     </canvas>
   );
 }
