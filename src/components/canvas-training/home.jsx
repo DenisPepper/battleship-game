1c991776fe663e11920047fcf2db7e4fe9fc79f6
DenisPepper <thech@yandex.ru>
2024-07-16 21:29:56 +0300

    Добавит CanvasRenderingContext2D

--- src/components/canvas-training/home.jsx
+++ src/components/canvas-training/home.jsx
@@ -1,8 +1,22 @@
+import { useRef, useCallback } from 'react';
+
 import './home.css';
 
 export function Home() {
+  const ctxRef = useRef();
+
+  const canvasRef = useCallback((canvas) => {
+    if (canvas === null) return;
+    ctxRef.current = canvas.getContext('2d');
+  }, []);
+
   return (
-    <canvas className='canvas-container' width={500} height={500}>
+    <canvas
+      className='canvas-container'
+      width={500}
+      height={500}
+      ref={canvasRef}
+    >
       A visual design tool for the interior filling of the cabinet.
     </canvas>
   );
