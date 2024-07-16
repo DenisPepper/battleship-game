318ca49d7ad418a269bac6438286fd114572f77b
DenisPepper <thech@yandex.ru>
2024-07-16 22:33:23 +0300

    Перенесет работу с контекстом рисования в отдельный файл

--- src/components/canvas-training/home.jsx
+++ src/components/canvas-training/home.jsx
@@ -1,6 +1,6 @@
 import { useRef, useCallback, useState, useEffect } from 'react';
-
 import './home.css';
+import { drawMDNsample } from './util/draw';
 
 export function Home() {
   const [isAvailable, setIsAvailable] = useState(true);
@@ -19,13 +19,7 @@ export function Home() {
   }, []);
 
   useEffect(() => {
-    const ctx = ctxRef.current;
-
-    ctx.fillStyle = 'rgb(200 0 0)';
-    ctx.fillRect(10, 10, 50, 50);
-
-    ctx.fillStyle = 'rgb(0 0 200 / 50%)';
-    ctx.fillRect(30, 30, 50, 50);
+    drawMDNsample(ctxRef.current);
   }, []);
 
   if (!isAvailable) return <div>Your browser do not support canvas API!</div>;
---
+++ src/components/canvas-training/util/draw.js
@@ -0,0 +1,7 @@
+export const drawMDNsample = (ctx) => {
+  ctx.fillStyle = 'rgb(200 0 0)';
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = 'rgb(0 0 200 / 50%)';
+  ctx.fillRect(30, 30, 50, 50);
+};
