08614505c4f989ebc51e2bec915a5cd927e12c01
DenisPepper <thech@yandex.ru>
2024-07-19 17:57:45 +0300

    Доработает механизм сокрытия контекстного меню

--- src/components/div/context-menu/context-menu.jsx
+++ src/components/div/context-menu/context-menu.jsx
@@ -1,12 +1,16 @@
 import './context-menu.css';
 
+const X_AXIS_SHIFT = 10;
+const Y_AXIS_SHIFT = 10;
+
 export function ContextMenu(props) {
   const { coords, closeMenu, addItem } = props;
 
   return (
     <menu
       className='dragger__ctx-menu'
-      style={{ top: coords.y, left: coords.x }}
+      style={{ top: coords.y - Y_AXIS_SHIFT, left: coords.x - X_AXIS_SHIFT }}
+      onMouseLeave={closeMenu}
     >
       <button type='button' onClick={() => addItem('vertical')}>
         Вертикальная перегородка
