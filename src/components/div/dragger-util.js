65ecfaf86542e99173918b98ae10e279b6c86f69
DenisPepper <thech@yandex.ru>
2024-07-23 01:03:23 +0300

    Создаст классы элементов dragger

--- src/components/div/dragger-util.js
+++ src/components/div/dragger-util.js
@@ -18,7 +18,7 @@ export const createAreaAfter = (area, id, width, pThickness) => ({
 
 export const createNewVertical = (area, id, pThickness, width) => ({
   id,
-  orientation: 'vertical', 
+  orientation: 'vertical',
   width: pThickness,
   height: area.height,
   top: area.top,
@@ -26,3 +26,71 @@ export const createNewVertical = (area, id, pThickness, width) => ({
 });
 
 // TODO: переписать на класс DraggerManager
+
+export class Partition {
+  id = null;
+  length = null;
+  thickness = null;
+  top = null;
+  left = null;
+
+  constructor(id, length, thickness, top, left) {
+    this.id = id;
+    this.length = length;
+    this.thickness = thickness;
+    this.top = top;
+    this.left = left;
+  }
+
+  /* get some() {
+    return this.#field;
+  } */
+}
+
+export class VerticalPartition extends Partition {
+  orientation = 'VERTICAL'; // 'VERTICAL' | 'HORIZONTAL'
+  left = [];
+  right = [];
+
+  constructor(id, length, thickness, top, left) {
+    super(id, length, thickness, top, left);
+  }
+}
+
+export class HorizontalPartition extends Partition {
+  orientation = 'HORIZONTAL'; // 'VERTICAL' | 'HORIZONTAL'
+  above = [];
+  under = [];
+
+  constructor(id, length, thickness, top, left) {
+    super(id, length, thickness, top, left);
+  }
+}
+
+export class Area {
+  id = null;
+  top = null;
+  left = null;
+  width = null;
+  height = null;
+  removeSelectionCallback = null;
+
+  constructor(id, width, height, top, left) {
+    this.id = id;
+    this.width = width;
+    this.height = height;
+    this.top = top;
+    this.left = left;
+  }
+
+  setRemoveSelectionCallback(cb) {
+    this.removeSelectionCallback = cb;
+  }
+
+  removeSelection() {
+    this.removeSelectionCallback();
+  }
+}
+
+const horizontal = new HorizontalPartition(1, 400, 16, 0, 0);
+console.log(horizontal);
