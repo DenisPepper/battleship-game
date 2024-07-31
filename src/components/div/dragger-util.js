fe68e01f46b81d37c2d3b9900e271ad5bcd854d4
DenisPepper <thech@yandex.ru>
2024-07-31 20:23:00 +0300

    Удалит неиспользуемый код в модуле прикладного функционала

--- src/components/div/dragger-util.js
+++ src/components/div/dragger-util.js
@@ -1,30 +1,3 @@
-/**
- * Функция принимает габарит области и возвращает новое уменьшенное значение габарита области.
- * При делении области перегородкой образуются две новых уменьшеных области.
- * @param {number} space ширина/высота области
- * @param {number} pThickness толщина перегородки (partition thickness)
- * @returns {number}
- */
-export const reduceArea = (space, pThickness) => (space - pThickness) / 2;
-
-export const createAreaBefore = (area, id, width) => ({ ...area, id, width });
-
-export const createAreaAfter = (area, id, width, pThickness) => ({
-  ...area,
-  id,
-  left: area.left + width + pThickness,
-  width,
-});
-
-export const createNewVertical = (area, id, pThickness, width) => ({
-  id,
-  orientation: 'vertical',
-  width: pThickness,
-  height: area.height,
-  top: area.top,
-  left: area.left + width,
-});
-
 export class DividerManager {
   thickness = null;
 
@@ -34,7 +7,8 @@ export class DividerManager {
 
   getHalfOf({ niche, orientation }) {
     if (orientation === 'vertical') return (niche.width - this.thickness) / 2;
-    if (orientation === 'horizontal') return (niche.height - this.thickness) / 2;
+    if (orientation === 'horizontal')
+      return (niche.height - this.thickness) / 2;
   }
 
   splitByVerticalPanel({ halfSize, niche, orientation, id1, id2, panelId }) {
