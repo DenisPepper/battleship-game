a6eef96cc23937b68cea5e64925b35318b3e9b3f
DenisPepper <thech@yandex.ru>
2024-10-25 01:00:37 +0300

    Добавит черновик формы расстановки кораблей

---
+++ src/game/components/add-form/add-form.jsx
@@ -0,0 +1,39 @@
+import './add-form.scss';
+
+export function AddForm() {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
+
+  return (
+    <form className='add-form' onSubmit={handleSubmit}>
+      <fieldset className='add-form__ship-type'>
+        <legend>type of ship</legend>
+        <label>
+          4
+          <input type='radio' name='ship-type' value={4} />
+        </label>
+        <label>
+          3
+          <input type='radio' name='ship-type' value={3} />
+        </label>
+        <label>
+          2
+          <input type='radio' name='ship-type' value={2} />
+        </label>
+        <label>
+          1
+          <input type='radio' name='ship-type' value={1} defaultChecked />
+        </label>
+      </fieldset>
+      <fieldset>
+        <legend>position</legend>
+        <select name='ship-position'>
+          <option value='vertical'>vertical</option>
+          <option value='horizontal'>horizontal</option>
+        </select>
+      </fieldset>
+      <button type='submit'>Add</button>
+    </form>
+  );
+}
--- src/game/components/battle-field/battle-field.jsx
+++ src/game/components/battle-field/battle-field.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Cell } from '../cell/cell';
 import './battle-field.scss';
+import { AddForm } from '../add-form/add-form';
 
 const buildCellsRow = (rowNumber) => ({ rowNumber, numbers: Array.from({ length: 10 }, (_, i) => i + 1) });
 const cells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(buildCellsRow);
@@ -23,13 +24,7 @@ export function BattleField(props) {
           );
         })}
         <div className='placement'>
-          <h2 className='placement__header'>Add a ship to the field:</h2>
-          <div className='placement__controls'>
-            <div className='placement__ship ship--4' draggable></div>
-            <div className='placement__ship ship--3'></div>
-            <div className='placement__ship ship--2'></div>
-            <div className='placement__ship ship--1'></div>
-          </div>
+          <AddForm />
         </div>
       </section>
     </main>
